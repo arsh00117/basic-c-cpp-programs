315badddaf160351366bf17780bf5abb161b7640
Arshdeep Singh <arsh250827@gmail.com>
2025-12-28 03:17:36 +0530

    basic calculator for two numbers in C++

---
+++ cpp/calculation of 2 number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    
+    cout<<"CALCULATION OF 2 NUMBER"<<"\n"<<endl;
+    cout<<"Enter first number : ";
+    cin>>a;
+    
+    cout<<"Enter Second number : ";
+    cin>>b;
+    
+    cout<<"\nSum is = "<<(a+b)<<endl;
+    cout<<"Diffrence is = "<<(a-b)<<endl;
+    cout<<"Multiply is = "<<(a*b)<<endl;    
+    cout<<"Divistion = "<<(a/b)<<endl;  
+    cout<<"Modulo = "<<(a % b)<<endl; 
+    
+    return 0;
+}
