f724fce4c3072ed10777eaa9065972a2c0d002da
arsh00117 <arsh250827@gmail.com>
2025-12-28 02:09:52 +0530

    check prime number checking program in C
    This program checks if a number is prime or not based on user input.

---
+++ check prime number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>              //header files
+
+int main() {                    //main function
+
+    int n,i;                    //variables declaration
+    int p = 0;                  //default values
+    int np = 0;
+    
+    printf("Enter a number to check:. ");   //entering number
+    scanf("%i",&n);
+    
+    for(i=2;i<=n-1;i++) {      //loop starts to check its prime or not
+        if(n%i==0) {
+            np = 1;
+            break;             // pauses if its not prime
+        } else {
+            p = 1;
+        }
+    }
+    if(np==1) {
+            printf("The number %i is not prime number.",n);
+        } else {
+            printf("The number %i is prime number.",n);
+        }
+    return 0;
+}
