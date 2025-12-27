c9e49af9f73ce7365b025153e6a74867999b7549
Arshdeep Singh <arsh250827@gmail.com>
2025-12-28 05:28:36 +0530

    Loop inside loop program to print square pattern of asterisks
    This program prints a square pattern of asterisks based on the value of m.
    
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

---
+++ cpp/loop-loop pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m = 5;
+    int x,y;
+    
+   for(y=1;y<=m;y++) {
+       for(x=1;x<=m;x++) {
+            cout<<"*";
+        }
+    cout<<endl;
+   }
+    return 0;
+}
