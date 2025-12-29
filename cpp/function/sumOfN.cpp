d2669f5034e694497779279a2795f502cef869c0
Arshdeep Singh <arsh250827@gmail.com>
2025-12-29 19:23:54 +0530

    sum

---
+++ cpp/function/sumOfN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int sumOfN(int n) {
+    int s = 0;
+    for(int i=1;i<=n;i++){
+       s= s+i;
+    }
+    return s;
+}
+
+int main() {
+    int a;
+    cout<<"****To Calculate Sum of N****"<<endl;
+    cout<<"Enter Number N : ";
+    cin>>a;
+    
+    cout<<"Sum Of N numbers are: "<<sumOfN(a)<<".";
+    return 0;
+}
