8e4c13a9f6a13f9f46762dd5ae375b2aabc0124b
Arshdeep Singh <arsh250827@gmail.com>
2025-12-29 20:03:04 +0530

    Square pattern using c++
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

--- cpp/patterns/square pattern.cpp
+++ cpp/patterns/square pattern.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     int n = 3;
-     int num = 1;
+    int num = 1;
+    
     for(int i = 1;i<=n;i++) {
         for(int j= 1;j<=n;j++) {
             cout<<num<<" ";
@@ -11,5 +12,6 @@ int main() {
         }
         cout<<endl;
     }
+    
     return 0;
 }
