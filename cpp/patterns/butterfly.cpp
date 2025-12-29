c12b36b9e427f05d6a37e8087d8d05280781a1db
Arshdeep Singh <arsh250827@gmail.com>
2025-12-29 18:29:47 +0530

    Butterfly pattern printing program in C++
    This program prints a butterfly pattern using asterisks.
    
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

---
+++ cpp/patterns/butterfly.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n = 4;
+
+    for(int i = 1;i<=n;i++) {
+        for(int j = 1;j<=i;j++) {
+            cout<<"*";
+        }
+        for(int j = 1;j<=2*(n-i);j++) {
+            cout<<" ";
+        }
+        for(int j = 1;j<=i;j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(int i = 1;i<=n-1;i++) {
+        for(int j = n-1;j>=i;j--) {
+            cout<<"*";
+        }
+        for(int j = 1;j<=2*i;j++) {
+            cout<<" ";
+        }
+        for(int j = n-1;j>=i;j--) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
