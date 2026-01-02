cfd32eb1f7e7fe5f80b4481d0b341c12b4ebac0e
Arshdeep Singh <arsh250827@gmail.com>
2026-01-02 18:46:42 +0530

    simple vector program in cpp
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

---
+++ cpp/vectors/simple vector storing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector <char> vec = {'a','b','c'};
+    for(char value : vec) { 
+        cout<<value<<" ";
+    } 
+
+    return 0;
+}
