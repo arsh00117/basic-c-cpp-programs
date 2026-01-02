80fa1e2f4038f9d21b6f915769d63be81a417863
Arshdeep Singh <arsh250827@gmail.com>
2026-01-02 15:42:24 +0530

    Largest and smallest number finding in an array
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

---
+++ cpp/array/largest_smallest.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+
+
+using namespace std;
+
+int main() {
+    int num[]={2,6,22,-45,23,-15};
+    int size = 6;
+    
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+    
+    for(int i = 0;i<size;i++) {
+        if(num[i]<smallest) {
+            smallest = num[i];
+        }
+    }
+    for(int i = 0;i<size;i++) {
+        if(num[i]>largest) {
+            largest = num[i];
+        }
+    }
+    cout<<"Smallest : "<<smallest<<endl;
+    cout<<"Largest : "<<largest<<endl;
+    return 0;
+    
+}
