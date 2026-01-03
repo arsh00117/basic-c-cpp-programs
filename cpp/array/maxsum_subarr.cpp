5166d012d2ada2d0c366e5e3c2ac5e787a4f6a9b
Arshdeep Singh <arsh250827@gmail.com>
2026-01-04 00:55:33 +0530

    maximum sum of subarray in cpp
    Signed-off-by: Arshdeep Singh <arsh250827@gmail.com>

--- cpp/array/maxsum_subarr.cpp
+++ cpp/array/maxsum_subarr.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+
 using namespace std;
 
 int main() {
     int n = 7;
     int array[7] = {3,-4,5,4,-1,7,-8};
-    int sum = 0;
+    
+    int maxSum = INT_MIN;
 
     for(int strt=0;strt<n;strt++) {
+        int currSum = 0;
         for(int end=strt;end<n;end++) {
-            sum += array[end];
+            currSum += array[end];
+            maxSum = max(currSum,maxSum);
         }
     }
-    cout<<"sum of subarray is : "<<sum<<endl;
+    cout<<"Maximum sum of subarray is : "<<maxSum<<endl;
     return 0;
 }
